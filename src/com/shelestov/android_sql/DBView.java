e9627c5b34b23813c24300c12a866cae469d9c49
Andrew Shelestov <streamx3@gmail.com>
2014-05-15 23:57:32 +0300

    + Added activity to view database

---
+++ src/com/shelestov/android_sql/DBView.java
@@ -0,0 +1,14 @@
+package com.shelestov.android_sql;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by athanaziz on 5/15/14.
+ */
+public class DBView extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dbview);
+    }
+}
